03a14a40ea4327aabcd53792297fc194a059fb92
seira <seira@DIVE-INTO-CODE.local>
2017-09-12 13:28:45 +0900

    0912

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,13 @@
 class PoemsController < ApplicationController
+
   def index
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    @poem = Poem.find(params[:id])
+    render json: @poems
+  end
+
 end
